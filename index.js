644863829fe68c7557804200c66497178fbf7fba
biagioni84 <biagioni84@gmail.com>
2018-04-02 16:01:28 -0300

    only proxy request

--- index.js
+++ index.js
@@ -1,117 +1,50 @@
-var api = require('./api.js');
+const https = require('https');
 exports.handler = function (request, context) {
-    log("DEGUG:", "Alexa request",  JSON.stringify(request));
-    if (request.directive.header.namespace === 'Alexa.Discovery' && request.directive.header.name === 'Discover') {
-        handleDiscovery(request, context, "");
-    }
-    else if (request.directive.header.namespace === 'Alexa.PowerController') {
-        if (request.directive.header.name === 'TurnOn' || request.directive.header.name === 'TurnOff') {
-            // log("DEBUG:", "TurnOn or TurnOff Request", JSON.stringify(request));
-            handlePowerControl(request, context);
-        }
-    }
-    else if (request.directive.header.namespace === 'Alexa.LockController') {
-        handleLockControl(request, context);
-    }
-    function handleDiscovery(request, context) {
-        var requestToken = request.directive.payload.scope.token;
-        var list = api.getDeviceList(requestToken, function(res){
-                res.on('data', function(d) {
+    log("DEBUG:", "Alexa request",  JSON.stringify(request));
+    handleRequest(request,context);
+    function handleRequest(request, context) {
+        // var requestToken = request.directive.payload.scope.token;
+        var list = proxyCommand(request, function(res){
+                res.on('data', function(d) {    
                 var payload = JSON.parse(d.toString('utf-8'));
-                var header = request.directive.header;
-                header.name = "Discover.Response";
-                log("DEBUG", "Discovery Response: ", JSON.stringify({ header: header, payload: payload }));
-                context.succeed({ event: { header: header, payload: payload } });
+                log("DEBUG: ", "Server response", JSON.stringify( payload));
+
+                // var header = request.directive.header;
+                // header.name = "Discover.Response";
+                // log("DEBUG", "Discovery Response: ", JSON.stringify({ header: header, payload: payload.payload }));
+                context.succeed(payload);
                 });
         });
     }
-
     function log(message, message1, message2) {
         console.log(message + message1 + message2);
     }
-    function handleLockControl(request, context){
-        var command ={};
-        var id = request.directive.endpoint.endpointId;
-        if (request.directive.header.name === 'Lock' ){
-        command = {url: "/device/" + id + "/execute/close"};
-        }else if(request.directive.header.name === 'Unlock') {
-        command = {url: "/device/" + id + "/execute/open"};
-        }else{
-            // TODO: respond command not handled
+    function proxyCommand(request,success){
+      var options = {
+        host: '34.227.234.171',
+        port: 443,
+        rejectUnauthorized: false, // an insecure way to deal with self signed certs
+        path: '/smarthome-skill',
+        method: 'POST',
+        headers: {
+            "Content-Type" : "application/json",
+            accept: '*/*'
         }
-        var requestToken = request.directive.endpoint.scope.token;
-        var sendCommand = api.sendCommand(requestToken, command, function(res){
-                res.on('data', function(d) {
-                var resp = JSON.parse(d.toString('utf-8'));
-                var responseHeader = request.directive.header;
-                responseHeader.namespace = "Alexa";
-                responseHeader.name = "Response";
-                responseHeader.messageId = responseHeader.messageId + "-R";
-                var c = {
-                     properties: [{
-                        namespace: "Alexa.LockController",
-                        name: "lockState",
-                        value: resp.lockState,
-                        timeOfSample: new Date(),
-                        uncertaintyInMilliseconds: 1000
-                     }]
-                };
-                var response = {
-                    context: c,
-                    event: {header: responseHeader,
-                            endpoint: request.directive.endpoint,
-                            payload: {}
-                    },
-                };
-                log("DEBUG", "Alexa.LockController ", JSON.stringify(response));
-                context.succeed(response);
-                });
-        });
+      };
+      if(request.directive.payload.scope && request.directive.payload.scope.token){
+       options.headers.token = request.directive.payload.scope.token;
+      }else{
+        options.headers.token = request.directive.endpoint.scope.token;
+      }
+      console.log(options);
+      var body = JSON.stringify(request);
+      var req = https.request(options, success);
+      req.write(body);
+      req.end();
+      req.on('error', function(e) {
+        console.error(e);
+      });
+    return "NO RETURN";
     }
-    function handlePowerControl(request, context) {
-        // get device ID passed in during discovery
-        var requestMethod = request.directive.header.name;
-        // get user token pass in request
-        var requestToken = request.directive.endpoint.scope.token;
-        var powerResult;
-        var command ={};
-        
-        var id = request.directive.endpoint.endpointId;
 
-        if (requestMethod === "TurnOn") {
-            command = {url: "/device/" + id + "/execute/on"};
-        }
-       else if (requestMethod === "TurnOff") {
-            command = {url: "/device/" + id + "/execute/off"};
-        }
-
-        var sendCommand = api.sendCommand(requestToken, command, function(res){
-                res.on('data', function(d) {
-                var resp = JSON.parse(d.toString('utf-8'));
-                var responseHeader = request.directive.header;
-                responseHeader.namespace = "Alexa";
-                responseHeader.name = "Response";
-                responseHeader.messageId = responseHeader.messageId + "-R";
-                var c = {
-                     properties: [{
-                        namespace: "Alexa.PowerController",
-                        name: "powerState",
-                        value: resp.powerState,
-                        timeOfSample: new Date(),
-                        uncertaintyInMilliseconds: 1000
-                     }]
-                };
-                var response = {
-                    context: c,
-                    event: {header: responseHeader,
-                            endpoint: request.directive.endpoint,
-                            payload: {}
-                    },
-                };
-                log("DEBUG", "Alexa.PowerController ", JSON.stringify(response));
-                context.succeed(response);
-                });
-        });
-
-    }
 };
